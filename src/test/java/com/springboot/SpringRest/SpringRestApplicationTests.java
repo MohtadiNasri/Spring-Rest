dc588721625d1d34ff2ede34e86959bfa5fa15b5
nasrimohtadi@gmail.com <Awkward@009>
2020-04-28 15:31:11 +0200

    Initial commit

---
+++ src/test/java/com/springboot/SpringRest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.SpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
